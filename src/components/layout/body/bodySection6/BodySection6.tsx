13b00f22276c255cc961d4cd60254fbb0b12ed5f
farazmn <farazmnm@gmail.com>
2024-12-09 12:37:32 +0330

    ranking deploy

--- src/components/layout/body/bodySection6/BodySection6.tsx
+++ src/components/layout/body/bodySection6/BodySection6.tsx
@@ -2,66 +2,61 @@ import ArticleCard, { DataObj } from '@/components/common/bodyComponents/article
 import './BodySection6.css'
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-// @ts-expect-error This type is incompatible due to library limitations.
-import AOS from 'aos';
-import 'aos/dist/aos.css';
 
 export default function BodySection6(){
     const [Data, setData] = useState<DataObj[]>([]);
 
-    // const fetchData = async ()=>{
-        // try{
-        // const response = await axios.get('https://ranking-game-wc9n.vercel.app/xmlTest/XMLFILE.xml');
-        // const xmlData = response.data;
-// 
-        // const parser = new DOMParser();
-        // const xmlDoc = parser.parseFromString(xmlData, "text/xml");
-        // console.log(xmlDoc)
-        // const items = Array.from(xmlDoc.getElementsByTagName("item"));
-        // const latestitems = items.slice(0,4);
-    //   
-        // latestitems.forEach((item) => {
-            // const Title = item.getElementsByTagName("title")[0].textContent;
-            // const Url = item.getElementsByTagName("link")[0].innerHTML;
-            // const date = item.getElementsByTagName("pubDate")[0].innerHTML;
-            // const dateObj = new Date(date);
-            // const year = dateObj.getFullYear();
-            // const month = dateObj.getMonth() + 1;
-            // const day = dateObj.getDate();
-            // const sendData = `${year}/${month}/${day}`
-            // const Category = item.getElementsByTagName("category")[0].textContent;
-            // let imgSrc;
-            // const content = item.getElementsByTagNameNS(
-                // "http://purl.org/rss/1.0/modules/content/",
-                // "encoded"
-            // )[0]?.textContent;
-            // 
-            // if (content) {
-                // const contentDoc = new DOMParser().parseFromString(content, "text/html");
-                // const figureImg = contentDoc.querySelector("figure img");
-                // imgSrc = figureImg?.getAttribute("src");
-            // }
-            // 
-            // Data.push({
-                // newTitle:Title,
-                // url : Url ,
-                // date : sendData ,
-                // category : Category,
-                // image : imgSrc ?? "default-image.jpg"
-            // })
-        // });
-        // const SendData = Data.slice(0,4);
-        // setData(SendData)
-        // }catch(err){
-            // console.log(err)
-        // }
-    // }
+    const fetchData = async ()=>{
+        try{
+        const response = await axios.get('https://ranking-game-wc9n.vercel.app/xmlTest/XMLFILE.xml');
+        const xmlData = response.data;
 
-    console.log(Data)
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlData, "text/xml");
+        console.log(xmlDoc)
+        const items = Array.from(xmlDoc.getElementsByTagName("item"));
+        const latestitems = items.slice(0,4);
+      
+        latestitems.forEach((item) => {
+            const Title = item.getElementsByTagName("title")[0].textContent;
+            const Url = item.getElementsByTagName("link")[0].innerHTML;
+            const date = item.getElementsByTagName("pubDate")[0].innerHTML;
+            const dateObj = new Date(date);
+            const year = dateObj.getFullYear();
+            const month = dateObj.getMonth() + 1;
+            const day = dateObj.getDate();
+            const sendData = `${year}/${month}/${day}`
+            const Category = item.getElementsByTagName("category")[0].textContent;
+            let imgSrc;
+            const content = item.getElementsByTagNameNS(
+                "http://purl.org/rss/1.0/modules/content/",
+                "encoded"
+            )[0]?.textContent;
+            
+            if (content) {
+                const contentDoc = new DOMParser().parseFromString(content, "text/html");
+                const figureImg = contentDoc.querySelector("figure img");
+                imgSrc = figureImg?.getAttribute("src");
+            }
+            
+            Data.push({
+                newTitle:Title,
+                url : Url ,
+                date : sendData ,
+                category : Category,
+                image : imgSrc ?? "default-image.jpg"
+            })
+        });
+        const SendData = Data.slice(0,4);
+        setData(SendData)
+        }catch(err){
+            console.log(err)
+        }
+    }
+
 
     useEffect(()=>{
-        // fetchData();
-        AOS.init();
+        fetchData();
     },[])
 
     return(
